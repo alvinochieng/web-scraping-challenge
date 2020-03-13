b6d1d62f9f14cf06d2dec749078dd479ce042593
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-03-12 20:25:08 -0400

    finally got something to work

--- Missions_to_mars/app.py
+++ Missions_to_mars/app.py
@@ -19,7 +19,7 @@ def scrape():
     
     mongo.db.collection.update({}, mars_data, upsert=True)
     
-    return redirect("/")
+    return redirect("/", code=302)
 
 if __name__=="__main__":
     app.run(debug=True)
--- Missions_to_mars/scrape_mars.py
+++ Missions_to_mars/scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup 
 import time
+import requests
 import pandas as pd
 
 def init_browser():
@@ -13,22 +14,46 @@ def scrape():
     
     url = 'https://mars.nasa.gov/news'
     browser.visit(url)
+    response = requests.get(url)
     
-    time.sleep(10)
+    time.sleep(2)
     
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
     
-    news_title = soup.find('div', class_='image_and_description_container').a.get_text()
+    news_title = soup.find('div', class_='content_title').get_text()
     
     news_p = soup.find('div', class_='rollover_description_inner').get_text()
     
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(url)
+    response = requests.get(url)
+    
+    time.sleep(2)
+    
+    mars_weather_tweet = []
+    soup = BeautifulSoup(response.text, "html.parser")    
+    
+    tweets = soup.find_all('div', class_='js-tweet-text-container')
+    #twitter = soup.find('p').get_text()
+     
+    for data in tweets:
+        twitter = data.find('p').text
+        
+        if 'sol' and 'pressure' in twitter:
+            mars_weather = twitter
+            break
+        else:
+            pass
+    
+    mars_weather_tweet.append(twitter)
     
     mars_data = {
         "headline": news_title,
-        "paragraph": news_p
-    }
-    
+        "paragraph": news_p,
+        "weather": mars_weather_tweet
+    }    
+        
     browser.quit()
     
     return mars_data
